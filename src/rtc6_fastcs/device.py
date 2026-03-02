7d4883d208ec8eafe835256056ef0fb7ccb33ca2
co2e14 <christian.orr@diamond.ac.uk>
2026-03-02 13:20:53 +0000

    refactor: reorganize imports and improve comments in device.py for clarity

--- src/rtc6_fastcs/device.py
+++ src/rtc6_fastcs/device.py
@@ -1,21 +1,22 @@
-from ophyd_async.core import StandardReadable, AsyncStageable
 from bluesky.protocols import Triggerable
+from ophyd_async.core import AsyncStageable, AsyncStatus, StandardReadable
 from ophyd_async.epics.core import (
-    epics_signal_rw,
     epics_signal_r,
-    epics_signal_x,
+    epics_signal_rw,
     epics_signal_w,
+    epics_signal_x,
 )
-from ophyd_async.core import AsyncStatus
 
 
 class Rtc6ControlSettings(StandardReadable):
     def __init__(self, prefix: str = "CONTROL:", name: str = "") -> None:
         super().__init__(name)
         with self.add_children_as_readables():
-            # TODO: make a python Enum to match the c++ enum so that we can limit this to the allowed values
+            # TODO: make a python Enum to match the c++ enum so that we can
+            # limit this to the allowed values
             self.laser_mode = epics_signal_rw(str, prefix + "LaserMode")
-            # This should be split up and made nicer if it is every really used, but currently is always just set to 0
+            # This should be split up and made nicer if it is every really
+            # used, but currently is always just set to 0
             self.laser_control = epics_signal_rw(int, prefix + "LaserControl")
             self.jump_speed = epics_signal_rw(float, prefix + "JumpSpeed")
             self.mark_speed = epics_signal_rw(float, prefix + "MarkSpeed")
